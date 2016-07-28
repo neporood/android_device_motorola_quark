53b76b75bfb308f393ee79822ca01bf9d4731d41
neporood <neporood@live.com>
2016-07-28 06:18:00 -0500

    Update to work with Bliss

--- bliss.mk
+++ bliss.mk
@@ -9,5 +9,12 @@ $(call inherit-product, vendor/bliss/config/nfc_enhanced.mk)
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-PRODUCT_RELEASE_NAME := Moto MAXX
+PRODUCT_RELEASE_NAME := DROID Turbo
 PRODUCT_NAME := bliss_quark
+PRODUCT_DEVICE := quark
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_DISPLAY_ID='bliss_quark-userdebug $(PLATFORM_VERSION) $(BUILD_ID) $(BUILD_NUMBER) test-keys' \
+    TARGET_BUILD_FLAVOR=bliss_quark-userdebug
