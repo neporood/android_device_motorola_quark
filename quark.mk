8b6fb395edcb418d47d8a4c036d884b992d74206
Nepo Rood <neporood@users.noreply.github.com>
2016-08-23 10:32:22 -0500

    Update and rename aicp.mk to quark.mk

--- quark.mk
+++ quark.mk
@@ -1,10 +1,14 @@
+# This file needs to me moved before compiling
+# My move command:
+# mv /home/nepo/aicp/vendor/aicp/products/quark.mk
+
 $(call inherit-product, device/motorola/quark/full_quark.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/configs/common.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/aicp/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
